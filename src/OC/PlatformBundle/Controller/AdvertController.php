e4b90aa5aadc20116d09738a8379fe3bd12ef0cb
Quentin Picart <quentin.picart@gmail.com>
2018-12-04 15:12:53 +0100

    Feat: create AdvertController

---
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace OC\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdvertController extends Controller
+{
+    public function indexAction()
+    {
+        return new Response("Notre propre Hello World");
+    }
+}
