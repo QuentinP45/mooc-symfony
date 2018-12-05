305a60a5a54021441c536f598ceb0c759a9d0054
Quentin Picart <quentin.picart@gmail.com>
2018-12-05 16:15:19 +0100

    add message flash in addAction

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -3,6 +3,7 @@
 namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller
@@ -16,6 +17,18 @@ class AdvertController extends Controller
 
     public function viewAction($id)
     {
-        return new Response("Affichage de l'anonce d'id : " . $id);
+        return $this->render('@OCPlatform/Advert/view.html.twig', [
+            'id' => $id,
+        ]);
+    }
+
+    public function addAction(Request $request)
+    {
+        $session = $request->getSession();
+
+        $session->getFlashBag()->add('info', 'Annonce bien enregistrée');
+        $session->getFlashBag()->add('info', 'Oui oui, elle est bien enregistrée !');
+        
+        return $this->redirectToRoute('oc_platform_view', ['id' => 5]);
     }
 }
