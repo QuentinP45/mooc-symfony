f727797ee3842f4623015cfaca4aa9324a5a1d12
Quentin Picart <quentin.picart@gmail.com>
2018-12-10 11:04:33 +0100

    Feat add message flash in CoreBundle homepage

--- src/OC/CoreBundle/Controller/HomeController.php
+++ src/OC/CoreBundle/Controller/HomeController.php
@@ -3,11 +3,16 @@
 namespace OC\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        if ($request->query->get('tag') === 'contact') {
+            $session = $request->getSession()->getFlashBag()->add('info', 'Page de contact en construction, merci de revenir plus tard !');
+        }
+
         return $this->render('@OCCore/Home/index.html.twig');
     }
 }
