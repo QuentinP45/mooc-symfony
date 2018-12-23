8aad47707b6d9e5a9b6c85456d5e3ab41cd841fb
Quentin Picart <quentin.picart@gmail.com>
2018-12-23 15:27:05 +0100

    Refactor(Form/ImageType): replace url and alt by file

--- src/OC/PlatformBundle/Form/ImageType.php
+++ src/OC/PlatformBundle/Form/ImageType.php
@@ -4,7 +4,7 @@ namespace OC\PlatformBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ImageType extends AbstractType
@@ -15,8 +15,9 @@ class ImageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('url', TextType::class)
-            ->add('alt', TextType::class)
+            // ->add('url', TextType::class)
+            // ->add('alt', TextType::class)
+            ->add('file', FileType::class)
         ;
     }
     
