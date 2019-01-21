d3a9351070a4320582d7c17011544c03f4733ff9
Quentin Picart <quentin.picart@gmail.com>
2019-01-21 09:22:28 +0100

    Feat: create platform events class

---
+++ src/OC/PlatformBundle/Event/PlatformEvents.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace OC\Platform\Event;
+
+final class PlatformEvents
+{
+    // Add an event
+    // const  TRIGGER_EVENT_NAME = 'event_name'
+    
+    const POST_MESSAGE = 'oc_platform.post_message';
+}
